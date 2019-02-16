4fd5c31ef89c4a40bd837179e1ac45c6e6a63515
avoqueen <kai.anime@gmx.de>
2019-02-16 13:43:40 +0100

    Changed the Chart layout
    I switched from a ComboBox to a TabPane to
    choose the Diagram

--- src/application/ChartController.java
+++ src/application/ChartController.java
@@ -18,6 +18,8 @@ import javafx.scene.chart.XYChart.Series;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import processing.WhatsAppStatistic;
@@ -26,129 +28,91 @@ public class ChartController implements Initializable {
 
 	@FXML
 	private MenuBar menubar;
+	
+	@FXML
+	private TabPane tabpane;
 
 	@FXML
-	private BarChart<String, Number> barchart;
+	private BarChart<String, Number> barchart1;
 
 	@FXML
-	private CategoryAxis x;
-
-	@FXML
-	private NumberAxis y;
-
-	@FXML
-	private ComboBox<String> combobox;
+	private BarChart<String, Number> barchart2;
 
 	@FXML
 	private PieChart piechart;
 
-	private Series<String, Number> simpleData;
-	private ObservableList<Series<String, Number>> advancedData;
-	private ObservableList<PieChart.Data> pieData;
-
-	private String[] chartTitles = new String[] { 
-			"Share of Conversation",
-			"Time-Message Diagram [Simple]",
-			"Time-Message Diagram [Advanced]" };
-	
-	private String[] chartIdentifier = new String[] { 
-			"Pie-Chart",
-			"Simple Bar-Chart",
-			"Advanced Bar-Chart" };
+	private boolean loadedPieChart = false,
+			loadedBarChart1 = false,
+			loadedBarChart2 = false,
+			loadedStatistcis = false;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources)
 	{
 		System.out.println("Initialized Chart Controller!");
-
-		combobox.setItems(
-				FXCollections.observableArrayList("Simple Bar-Chart", "Advanced Bar-Chart", "Pie-Chart", "Statistics"));
-
-		combobox.getSelectionModel().select(0);
-
-		piechart.setVisible(false);
-
-		Series<String, Number> data = WhatsAppStatistic.getSimpleBarChartData(Main.getWhatsAppFile());
-		barchart.getData().add(data);
+		loadPieChart();
+	}
+	
+	void loadCharts() {
+		loadPieChart();
+		loadSimpleBarChart();
+		loadAdvancedBarChart();
+	}
+	
+	void reloadCurrentChart() {
+		final int index = tabpane.getSelectionModel().getSelectedIndex();
+		
+		switch(index)
+		{
+		case 0:
+			loadPieChart();
+			break;
+		case 1:
+			loadSimpleBarChart();
+			break;
+		case 2:
+			loadAdvancedBarChart();
+			break;
+		case 3:
+			break;
+		}
 	}
 
 	@FXML
-	void changeChart()
+	private void loadPieChart()
 	{
-		String selected = combobox.getSelectionModel().getSelectedItem();
+		if (loadedPieChart)
+		{
+			return;
+		}
+
+		loadedPieChart = true;
+		piechart.getData().addAll(WhatsAppStatistic.getPieChartData(Main.getWhatsAppFile()));
+
+	}
+
+	@FXML
+	private void loadSimpleBarChart()
+	{
+		if (loadedBarChart1)
+		{
+			return;
+		}
 		
-		if(selected == null) {
-			combobox.getSelectionModel().select(0);
-			changeChart();
-			return;
-		}
-
-		if (selected.equals(chartIdentifier[0]))
-			initializePieChart();
-		else if (selected.equals(chartIdentifier[1]))
-			initializeSimpleBarChart();
-		else if (selected.equals(chartIdentifier[2])) initializeAdvancedBarChart();
-
+		loadedBarChart1 = true;
+		barchart1.getData().add(WhatsAppStatistic.getSimpleBarChartData(Main.getWhatsAppFile()));
 	}
 
-	private void initializePieChart()
+	@FXML
+	private void loadAdvancedBarChart()
 	{
-		barchart.setVisible(false);
-		piechart.setVisible(true);
-		piechart.setTitle(chartTitles[0]);
-
-		if (pieData != null)
+		if(loadedBarChart2)
 		{
 			return;
 		}
-
-		pieData = WhatsAppStatistic.getPieChartData(Main.getWhatsAppFile());
-		piechart.getData().clear();
-		piechart.getData().addAll(pieData);
-
-	}
-
-	private void initializeSimpleBarChart()
-	{
-		barchart.setVisible(true);
-		piechart.setVisible(false);
-		barchart.setTitle(chartTitles[1]);
-
-		if (simpleData != null)
-		{
-			if (!barchart.getData().contains(simpleData))
-			{
-				barchart.getData().removeAll(advancedData);
-				barchart.getData().add(simpleData);
-			}
-			return;
-		}
-
-		simpleData = WhatsAppStatistic.getSimpleBarChartData(Main.getWhatsAppFile());
-		barchart.getData().clear();
-		barchart.getData().add(simpleData);
-	}
-
-	private void initializeAdvancedBarChart()
-	{
-		barchart.setVisible(true);
-		piechart.setVisible(false);
-		barchart.setTitle(chartTitles[2]);
-
-		// if (advancedData != null)
-		// {
-		// if (!barchart.getData().containsAll(advancedData))
-		// {
-		// barchart.getData().remove(simpleData);
-		// barchart.getData().addAll(advancedData);
-		// //barchart.getData().addAll(advancedData);
-		// }
-		// return;
-		// }
-
-		advancedData = WhatsAppStatistic.getAdvancedBarChartData(Main.getWhatsAppFile());
-		barchart.getData().clear();
-		barchart.getData().addAll(advancedData);
+		
+		loadedBarChart2 = true;
+		barchart2.getData().addAll(WhatsAppStatistic.getAdvancedBarChartData(Main.getWhatsAppFile()));
 	}
 
 	@FXML
@@ -160,8 +124,8 @@ public class ChartController implements Initializable {
 	@FXML
 	void openNewFile(ActionEvent event)
 	{
-		// abusing the chart to get the scene reference...
-		Stage stage = (Stage) barchart.getScene().getWindow();
+//		// abusing the chart to get the scene reference...
+		Stage stage = (Stage) piechart.getScene().getWindow();
 		File file;
 
 		FileChooser fileChooser = new FileChooser();
@@ -180,16 +144,20 @@ public class ChartController implements Initializable {
 
 		Main.initWhatsAppFile(file);
 		resetChartData();
-		changeChart();
+		reloadCurrentChart();
 	}
 
 	private void resetChartData()
 	{
-		simpleData = null;
-		advancedData = null;
-		pieData = null;
+		loadedPieChart = false;
+		loadedBarChart1 = false;
+		loadedBarChart2 = false;
+		
+		piechart.getData().clear();
+		barchart1.getData().clear();
+		barchart2.getData().clear();
 	}
-
+//
 	@FXML
 	void changetoGerman()
 	{
@@ -209,56 +177,50 @@ public class ChartController implements Initializable {
 
 		help.setText("Hilfe");
 		help.getItems().get(0).setText("Über");
-
-		chartTitles = new String[] { 
-				"Gesprächsanteile",
-				"Zeit-Nachrichten Diagramm [Einfach]",
-				"Zeit-Nachrichten Diagramm [Fortgeschritten]" };
 		
-		chartIdentifier = new String[] { 
-				"Kreisdiagramm",
-				"Einfaches Balkendiagramm",
-				"Fortgeschrittenes Balkendiagramm" };
+		tabpane.getTabs().get(0).setText("Kreisdiagramm");
+		piechart.setTitle("Gesprächsanteile");
 		
-		combobox.setItems(
-				FXCollections.observableArrayList(chartIdentifier[1], chartIdentifier[2], chartIdentifier[0], "Statistiken"));
+		tabpane.getTabs().get(1).setText("Einfaches Balkendiagramm");
+		barchart1.setTitle("Zeit-Nachrichten Diagramm [Einfach]");
 		
-		changeChart();
+		tabpane.getTabs().get(2).setText("Fortgeschrittenes Balkendiagramm");
+		barchart2.setTitle("Zeit-Nachrichten Diagramm [Fortgeschritten]");
 	}
-
+//
 	@FXML
 	void changetoEnglish()
 	{
-		Menu file = menubar.getMenus().get(0);
-		Menu settings = menubar.getMenus().get(1);
-		Menu help = menubar.getMenus().get(2);
-
-		file.setText("File");
-		file.getItems().get(0).setText("Open...");
-		file.getItems().get(2).setText("Quit");
-
-		settings.setText("Settings");
-		settings.getItems().get(0).setText("Language");
-		((Menu) settings.getItems().get(0)).getItems().get(0).setText("English");
-		((Menu) settings.getItems().get(0)).getItems().get(1).setText("German");
-		settings.getItems().get(2).setText("Size");
-
-		help.setText("Help");
-		help.getItems().get(0).setText("About");
-
-		chartTitles = new String[] { 
-				"Share of Conversation",
-				"Time-Message Diagram [Simple]",
-				"Time-Message Diagram [Advanced]" };
-		
-		chartIdentifier = new String[] { 
-				"Pie-Chart",
-				"Simple Bar-Chart",
-				"Advanced Bar-Chart" };
-		
-		combobox.setItems(
-				FXCollections.observableArrayList(chartIdentifier[1], chartIdentifier[2], chartIdentifier[0], "Statistics"));
-		
-		changeChart();
+//		Menu file = menubar.getMenus().get(0);
+//		Menu settings = menubar.getMenus().get(1);
+//		Menu help = menubar.getMenus().get(2);
+//
+//		file.setText("File");
+//		file.getItems().get(0).setText("Open...");
+//		file.getItems().get(2).setText("Quit");
+//
+//		settings.setText("Settings");
+//		settings.getItems().get(0).setText("Language");
+//		((Menu) settings.getItems().get(0)).getItems().get(0).setText("English");
+//		((Menu) settings.getItems().get(0)).getItems().get(1).setText("German");
+//		settings.getItems().get(2).setText("Size");
+//
+//		help.setText("Help");
+//		help.getItems().get(0).setText("About");
+//
+//		chartTitles = new String[] { 
+//				"Share of Conversation",
+//				"Time-Message Diagram [Simple]",
+//				"Time-Message Diagram [Advanced]" };
+//		
+//		chartIdentifier = new String[] { 
+//				"Pie-Chart",
+//				"Simple Bar-Chart",
+//				"Advanced Bar-Chart" };
+//		
+//		combobox.setItems(
+//				FXCollections.observableArrayList(chartIdentifier[1], chartIdentifier[2], chartIdentifier[0], "Statistics"));
+//		
+//		changeChart();
 	}
 }
